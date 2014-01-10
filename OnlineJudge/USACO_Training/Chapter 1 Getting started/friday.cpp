90eb00b75f53e98d8f78b8ebd014f9ef5303ad35
tangjz <t251346744@gmail.com>
2014-01-10 20:28:19 +0800

    Change Files

--- OnlineJudge/USACO_Training/Chapter 1 Getting started/friday.cpp
+++ OnlineJudge/USACO_Training/Chapter 1 Getting started/friday.cpp
@@ -12,23 +12,15 @@ int main()
 	scanf("%d", &n);
 	for(int i = 1900; i <= 1900 + n - 1; ++i)
 		for(int j = 0; j < 12; ++j)
+		{
 			if(j == 2)
-				if(i % 400 == 0 || i % 4 == 0 && i % 100 != 0)
-				{
-					w = (w + 1) % 7;
-					++used[w];
-				}
-				else ++used[w];
-			else if(j == 0 || j == 1 || j == 3 || j == 5 || j == 7 || j == 8 || j == 10)
 			{
-				w = (w + 3) % 7;
-				++used[w];
+				if(i % 400 == 0 || i % 4 == 0 && i % 100 != 0) w = (w + 1) % 7;
 			}
-			else
-			{
-				w = (w + 2) % 7;
-				++used[w];
-			}
+			else if(j == 0 || j == 1 || j == 3 || j == 5 || j == 7 || j == 8 || j == 10) w = (w + 3) % 7;
+			else w = (w + 2) % 7;
+			++used[w];
+		}
 	printf("%d %d %d %d %d %d %d\n", used[6], used[0], used[1], used[2], used[3], used[4], used[5]);
 	fclose(stdin);
 	fclose(stdout);
