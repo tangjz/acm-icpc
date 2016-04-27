2b2c69084a2736f4ef9a33d2157627ba0deb2c77
tangjz <t251346744@gmail.com>
2016-04-28 00:00:33 +0800

    inverse

--- hdu/5244.cpp
+++ hdu/5244.cpp
@@ -1,33 +1,14 @@
 #include <cstdio>
 const int maxn = 1 << 21;
-int t, n;
-long long a[maxn], b[maxn], c[maxn];
-void solve(int x1, int x2, int y1, int y2, long long* c)
-{
-	if(x1 == x2)
-	{
-		c[x1] = a[y1];
-		return;
-	}
-	int d = x2 - x1 + 1 >> 1;
-	int xm = x1 + d, ym = y1 + d;
-	solve(x1, xm - 1, y1, ym - 1, c);
-	solve(x1, xm - 1, ym, y2, c + d);
-	for(int i = 0; i < d; ++i)
-	{
-		b[x1 + i] = c[i] + c[d + i];
-		b[xm + i] = -c[i] + c[d + i];
-	}
-	for(int i = 0; i < d << 1; ++i)
-		c[i] = b[x1 + i];
-}
+int t, n, m;
+long long a[maxn], tmp;
 int main()
 {
 	scanf("%d", &t);
 	for(int Case = 1; Case <= t; ++Case)
 	{
-		scanf("%d", &n);
-		n = 1 << n;
+		scanf("%d", &m);
+		n = 1 << m;
 		for(int i = 0; i < n; ++i)
 			scanf("%lld", a + i);
 		printf("Case #%d:", Case);
@@ -36,10 +17,18 @@ int main()
 			printf(" %d\n", a[0]);
 			continue;
 		}
-		solve(0, n - 1, 0, n - 1, c);
-		c[0] -= (n >> 1) * a[n - 1];
+		tmp = a[n - 1];
+		for(int i = 1; i < n; i <<= 1)
+			for(int j = 0, k = i; j < n; j = k, k = j + i)
+				for(int o = 0; o < i; ++o)
+				{
+					long long t = a[j];
+					a[j++] += a[k];
+					a[k++] -= t;
+				}
+		a[0] -= tmp << m - 1;
 		for(int i = 0; i < n; ++i)
-			printf(" %lld", c[i] / (n >> 1));
+			printf(" %lld", a[i] >> m - 1);
 		putchar('\n');
 	}
 	return 0;
